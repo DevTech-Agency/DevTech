1cc31d807be51585a55637bfe9ce42802e26d733
DevTech-Agency <devtechagency.ec@gmail.com>
2025-06-06 15:41:51 -0500

    nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header"
+import Hero from "./components/Hero"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      {/*Gradient image*/}
+      <img className="absolute top-0 right-0 opacity-60 -z-10" 
+      src="/gradient.png" alt="Gradient-img" />
+
+      {/*Blur Effect */}
+      <div className="h-0 w-[40rem] absolute 
+      top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63]
+      -rotate-[30deg] -z-10"></div>
+
+      <Header/>
+      <Hero/>
+    </main>
   )
 }
-
-export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,95 @@
+import 'boxicons/css/boxicons.min.css';
+
+const Header = () => {
+    // Function to toggle the mobiel menu
+    const toggleMobileMenu = () => {
+        // Get the Mobile Menu Element
+        const mobileMenu = document.getElementById
+        ('mobileMenu')
+
+        // If it has the 'hidden' class, remove it, otherwise add it
+        if(mobileMenu.classList.contains('hidden')) {
+            mobileMenu.classList.remove('hidden');
+        } else {
+            mobileMenu.classList.add('hidden');
+        }
+    }
+
+  return (
+    <header className="flex justify-between items-center
+    py-4 px-4 lg:px-20">
+
+        <h1 className="text-3xl md:text-4xl lg:text-5xl
+        font-light m-0">
+            Dev
+        </h1>
+
+        {/*Desktop Navigation */}
+        <nav className="hidden md:flex items-center gap-12">
+            <a className="text-base tracking-wider 
+            transition-colors hover:text-gray-300
+            z-50" href="#">
+                Contacto
+            </a>
+
+            <a className="text-base tracking-wider 
+            transition-colors hover:text-gray-300
+            z-50" href="#">
+                Servicios
+            </a>
+
+            <a className="text-base tracking-wider 
+            transition-colors hover:text-gray-300
+            z-50" href="#">
+                Precios
+            </a>
+        </nav>
+
+        <button className="hidden md:block bg-[#a7a7a7] 
+        text-black py-3 px-8 rounded-full border-none font-medium
+        transition-all duration-500 hover:bg-white cursor-pointer z-50">
+            Cotizar
+        </button>
+
+        {/* Mobile Menu Button - Visible only on Mobile */}
+        <button onClick={toggleMobileMenu}
+        className='md:hidden text-3x1 p-2 z-50'>
+            <img className="top-0 left-0 z-10 filter brightness-0 invert"
+        src="/menu-left.png" alt="Mobile-menu" /> 
+        </button>
+
+        {/* Mobile Menu - Hidden by Default */}
+        <div id='mobileMenu' className='hidden fixed top-16 bottom-0 right-0
+        left-0 p-5 md:hidden z-40 bg-black bg-opacity-70
+        backdrop-blur-md'>
+            <nav className='flex flex-col gap-6 items-center'>
+                <a className="text-base tracking-wider 
+                transition-colors hover:text-gray-300
+                z-50" href="#">
+                    Contacto
+                </a>
+
+                <a className="text-base tracking-wider 
+                transition-colors hover:text-gray-300
+                z-50" href="#">
+                    Servicios
+                </a>
+
+                <a className="text-base tracking-wider 
+                transition-colors hover:text-gray-300
+                z-50" href="#">
+                Precios
+            </a>
+                <button className="md:block bg-[#a7a7a7] 
+                text-black py-3 px-8 rounded-full border-none font-medium
+                transition-all duration-500 hover:bg-white cursor-pointer z-50">
+                Cotizar
+        </button>
+            </nav>
+        </div>
+
+    </header>
+  )
+}
+
+export default Header
